5477819de613e1adbcacde76aabfe8e21b965a11
Augusto Morante <augustomorante@hotmail.com>
2022-06-08 18:32:12 -0500

    Create CommunityCardComponent.vue

---
+++ src/components/CommunityCardComponent.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="gamemenu-content-training-cards">
+    <div class="gamemenu-content-training-cards-img">
+      <img
+        :src="coverUrl"
+        :alt="name"
+      />
+    </div>
+    <h1>{{name}}</h1>
+    <p>
+      {{description}}
+    </p>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped></style>
